93db3190e829838be3c97c1d902df06b84096dd8
Fahim H <fahimhussain21@gmail.com>
2022-03-13 15:06:07 -0400

    added new command and polished shop

--- main.py
+++ main.py
@@ -49,13 +49,14 @@ async def inventory(ctx):
 
 from discord.ui import Button, Select, View
 @bot.slash_command()
-async def shop(ctx):
+async def shop(ctx: discord.ApplicationContext):
     options = []
     for item in ShopItem.objects:
         option = discord.SelectOption(label=item.item.name, description=item.item.price)
         options.append(option)
     menu = Select(placeholder="Select an item for purchase", options=options)
     async def on_select(interaction: discord.Interaction):
+        await ctx.interaction.delete_original_message()
         # we only have 1 value
         itemName = menu.values[0]
         itemPrice = Item.objects.get(name=itemName).price
@@ -64,13 +65,32 @@ async def shop(ctx):
         cancelButton = Button(style=discord.ButtonStyle.secondary, label="Cancel")
         confirmButton = Button(style=discord.ButtonStyle.success, label="Purchase")
 
-        async def on_confirm(interaction: discord.Interaction):
-            player = Player.objects.get(userId=interaction.user.id)
-            player.wallet -= itemPrice
-            player.items.append(Item.objects.get(name=itemName))
-            player.save()
-            await interaction.response.send_message(f"You purchased {itemName} for {itemPrice}")
+        async def on_cancel(interactione: discord.Interaction):
+            await interaction.response.defer(ephemeral=False)
+            await interaction.delete_original_message()
+
+        async def on_confirm(interactionne: discord.Interaction):
+            try:
+                player = Player.objects.get(userId=interactionne.user.id)
+            except DoesNotExist:
+                player = Player(userId=interactionne.user.id, displayName=interactionne.user.display_name, wallet=0, items=[])
+                player.save()
+            if Item.objects.get(name=itemName) in player.items:
+                # the double underscore is like items.name
+                await interactionne.response.send_message(f"You already own {itemName}")
+                await interaction.delete_original_message()
+            else:
+                if player.wallet < itemPrice:
+                    await interactionne.response.send_message(f"You don't have enough money to buy {itemName}")
+                    await interaction.delete_original_message()
+                    return
+                player.wallet -= itemPrice
+                player.items.append(Item.objects.get(name=itemName))
+                player.save()
+                await interactionne.response.send_message(f"You purchased {itemName} for {itemPrice}")
+                await interaction.delete_original_message()
         confirmButton.callback = on_confirm
+        cancelButton.callback = on_cancel
         view2 = View()
         view2.add_item(cancelButton)
         view2.add_item(confirmButton)
@@ -79,5 +99,18 @@ async def shop(ctx):
     view = View()
     view.add_item(menu)
     await ctx.respond(view=view)
+
+@bot.slash_command(guild_ids=[804100748275220530])
+async def sparechange(ctx: discord.ApplicationContext, amount: int = None):
+    try:
+        player = Player.objects.get(userId=ctx.author.id)
+    except DoesNotExist:
+        player = Player(userId=ctx.author.id, displayName=ctx.author.name, wallet=0, items=[])
+        player.save()
+    # if they didn't pass in an amount, the condition will fail and it'll give 10k
+    player.wallet += amount or 10000
+    player.save()
+    embed = discord.Embed(title="Money deposited", description=f"{amount or 10000} has been deposited into your wallet")
+    await ctx.respond(embed=embed)
 
 bot.run(token)
